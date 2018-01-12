f6aaa9de54b4feb0d90ff3773bc9be3e973356e2
Sarah Llewelyn <sllewelyn@renttherunway.com>
2018-01-11 20:58:35 -0500

    travel from scene to scene

--- Assets/MyAssets/MyScripts/ToScene2.cs
+++ Assets/MyAssets/MyScripts/ToScene2.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ToScene1 : MonoBehaviour {
+public class ToScene2 : MonoBehaviour {
 
 	public GameObject player;
 
 	void OnTriggerEnter(Collider objectCollided) {
 		if (objectCollided.gameObject == player.gameObject) {
 //			Debug.Log ("Change scene");
-			SceneManager.LoadScene("Scene_01");
+			SceneManager.LoadScene("Scene_02");
 		}
 	}
 
