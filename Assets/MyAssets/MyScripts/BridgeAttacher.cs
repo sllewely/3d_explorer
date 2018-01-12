8db220c81e78bdcb61fc62b00572869060e1b61e
Sarah Llewelyn <sllewelyn@renttherunway.com>
2018-01-11 20:26:44 -0500

    player moves with bridge

---
+++ Assets/MyAssets/MyScripts/BridgeAttacher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeAttacher : MonoBehaviour {
+
+	public GameObject bridge;
+	public GameObject player;
+
+	void OnTriggerEnter() {
+		player.transform.parent = bridge.transform;
+	}
+
+}
---
+++ Assets/MyAssets/MyScripts/BridgeUncouple.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeUncouple : MonoBehaviour {
+
+	public GameObject player;
+
+	void OnTriggerEnter() {
+		player.transform.parent = null;
+	}
+
+}
