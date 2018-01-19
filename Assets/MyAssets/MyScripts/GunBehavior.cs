ad72279b00fffe772438caae5ef4c0df60bec4cf
Sarah Llewelyn <sllewelyn@renttherunway.com>
2018-01-18 21:01:59 -0500

    enemy health and ray casting

---
+++ Assets/MyAssets/MyScripts/GunBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehavior : MonoBehaviour {
+
+	public float damage;
+	public float range;
+
+	public Camera playerCam;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire1")) {
+			FireRayCast ();
+		}
+		
+	}
+
+	RaycastHit hitInfo;
+
+	void FireRayCast() {
+		if (Physics.Raycast (playerCam.transform.position, playerCam.transform.forward, out hitInfo, range)) {
+			Debug.Log ("I hit something");
+			Debug.DrawRay (playerCam.transform.position, playerCam.transform.forward, Color.cyan);
+
+			Target_Enemy enemy = hitInfo.transform.GetComponent<Target_Enemy> ();
+
+			if (enemy != null) {
+				enemy.TakeDamage (damage);
+			}
+		} else {
+			Debug.Log ("Miss");
+		}
+	}
+}
---
+++ Assets/MyAssets/MyScripts/Target_Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target_Enemy : MonoBehaviour {
+
+	public float health;
+	public void TakeDamage(float damageAmount) {
+		health -= damageAmount;
+
+		if (health <= 0) {
+			Die ();
+		}
+	}
+
+	void Die() {
+		Destroy (gameObject);
+	}
+}
